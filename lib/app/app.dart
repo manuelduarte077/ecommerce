f7230d86d9299bf939ee04a2714cfa579bda6ce9
Manuel Duarte <carlosd99m@gmail.com>
2022-03-28 02:30:08 -0600

    chore: project setup

---
+++ lib/app/app.dart
@@ -0,0 +1,19 @@
+import 'package:ecommerce/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Material App Bar'),
+          ),
+          body: const HomeScreen()),
+    );
+  }
+}
---
+++ lib/screens/screens.dart
@@ -0,0 +1,1 @@
+export 'package:ecommerce/screens/home_screen.dart';
