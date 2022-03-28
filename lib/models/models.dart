dca6ab3168ad75cd60ceef1e56b727b26a9e3424
Manuel Duarte <carlosd99m@gmail.com>
2022-03-28 15:39:27 -0600

    :sparkles: add carousel card (#3)
    * :sparkles: add carousel card
    
    * feat: product carousel list
    
    * Update README.md

---
+++ lib/models/models.dart
@@ -0,0 +1,2 @@
+export 'package:ecommerce/models/category_model.dart';
+export 'package:ecommerce/models/product_model.dart';
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,8 @@
-import 'package:ecommerce/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+import 'package:ecommerce/models/models.dart';
+import 'package:ecommerce/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -15,9 +18,42 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CustomAppBar(title: "Unicorn Store"),
-      bottomNavigationBar: CustomNavBar(),
+    return Scaffold(
+      appBar: const CustomAppBar(title: "Unicorn Store"),
+      bottomNavigationBar: const CustomNavBar(),
+      body: Column(
+        children: [
+          Container(
+            child: CarouselSlider(
+              options: CarouselOptions(
+                aspectRatio: 1.5,
+                viewportFraction: 0.9,
+                enlargeCenterPage: true,
+                // autoPlay: true,
+                enlargeStrategy: CenterPageEnlargeStrategy.height,
+              ),
+              items: Category.categories
+                  .map((category) => HeroCarouselCard(category: category))
+                  .toList(),
+            ),
+          ),
+          const SectionTitle(
+            title: "Recommended",
+          ),
+          ProductCarousel(
+            products: Product.products
+                .where((product) => product.isRecommended)
+                .toList(),
+          ),
+          const SectionTitle(
+            title: "Most Popular",
+          ),
+          ProductCarousel(
+            products:
+                Product.products.where((product) => product.isPopular).toList(),
+          ),
+        ],
+      ),
     );
   }
 }
